ec4282c416def8265632ce4a66d7b20685c264ee
Veerabala <imveerabala@gmail.com>
2025-10-31 23:01:58 +0530

    first commit

---
+++ database.py
@@ -0,0 +1,10 @@
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+db_url  = os.getenv("DATABASE_URL")
+
+engine = create_engine(db_url)
+session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
